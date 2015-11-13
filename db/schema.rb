4062fd0bcfcbc62aaf5151d3f70a19c73de21a9f
Rob Holody <robholody@Robs-MacBook-Pro.local>
2015-11-13 01:18:23 -0500

    Backup code added from failures

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,4 @@ ActiveRecord::Schema.define(version: 20151113060245) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price_in_cents"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
 end
