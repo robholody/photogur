eca8350006024592ab11620d51072d0ceb1e78b7
Rob Holody <robholody@Robs-MacBook-Pro.local>
2015-11-13 01:04:24 -0500

    Creating Picture model in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113055316) do
+ActiveRecord::Schema.define(version: 20151113060245) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "artist"
+    t.string   "title"
+    t.string   "erl"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
